57abbaae31959af9c1d4e23c4eee56fba0e35396
David Arrebola <arrebola.work@gmail.com>
2025-04-20 12:51:59 +0200

    ejercicio realizado

--- index.js
+++ index.js
@@ -1,11 +1,89 @@
 //DESARROLLA AQUI TUS SOLUCIONES
+const getRandomPokemon = async () => {
+  try {
+    const id = Math.floor(Math.random() * 898) + 1;
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    throw new Error("ha habido un error", error);
+  }
+};
 
-async function getImageAndName (pokemon){
+const getImageAndName = async (name = "pikachu") => {
+  try {
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+    const data = await res.json();
+    return {
+      img: data.sprites.front_default,
+      name: data.name,
+    };
+  } catch (error) {
+    throw new Error("ha habido un error", error);
+  }
+};
+const printImageAndName = async () => {
+  try {
+    const { img, name } = await getImageAndName();
+    return `
+<section>
+    <img src="${img}" alt="${name}">
+    <h1>${name}</h1>
+</section>
+`.trim();
+  } catch (error) {
+    throw new Error("ha habido un error", error);
+  }
+};
 
-    let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
-    let data = await response.json();
-    let name = data.name;
-    let img = data.sprites.front_default;
-    return {name, img}
-    
-}
+const getRandomDogImage = async () => {
+  try {
+    const res = await fetch("https://dog.ceo/api/breeds/image/random");
+    const data = await res.json();
+    return data.message;
+  } catch (error) {
+    throw new Error("ha habido un error", error);
+  }
+};
+const getRandomPokemonImage = async () => {
+  try {
+    const id = Math.floor(Math.random() * 898) + 1;
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    const data = await res.json();
+    return data.sprites.front_default;
+  } catch (error) {
+    throw new Error("ha habido un error", error);
+  }
+};
+
+const getRandomCharacter = async () => {
+  try {
+    const id = Math.floor(Math.random() * 826) + 1;
+    const res = await fetch(`https://rickandmortyapi.com/api/character/${id}`);
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    throw new Error("ha habido un error", error);
+  }
+};
+const getRandomCharacterInfo = async () => {
+  try {
+    const character = await getRandomCharacter();
+
+    const episodeUrls = character.episode;
+    const firstEpisodeUrl = episodeUrls[0];
+
+    const firstEpRes = await fetch(firstEpisodeUrl);
+    const firstEpData = await firstEpRes.json();
+
+    return {
+      img: character.image,
+      name: character.name,
+      episodes: episodeUrls,
+      firstEpisode: firstEpData.name,
+      dateEpisode: firstEpData.air_date,
+    };
+  } catch (error) {
+    throw new Error("ha habido un error", error);
+  }
+};
